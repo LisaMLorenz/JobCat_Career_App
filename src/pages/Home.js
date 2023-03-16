9822c4f60355385feff45cece0350902c722d53c
Manonthemon <klimowicz.maciej@gmail.com>
2023-03-16 20:39:16 +0000

    Minor changes to home page buttons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,9 +7,9 @@ function Home() {
     <div>
         <h1>Welcome to JOBCAT</h1>
         <h3>With JobCat you can quickly create a professional resume, apply for your dream job and manage your job applications. Get to work!</h3>
-        <Link to="/resumeform"><button>Resume Form</button></Link>
-        <Link to="/jobsearch"><button>Job Search</button></Link>
-        <Link to="/projects"><button>Projects</button></Link>
+        <Link to="/resumeForm"><button>Generate Resume</button></Link>
+        <Link to="/jobSearch"><button>Job Search</button></Link>
+        <Link to="/JobApplicationHistory"><button>Application History</button></Link>
     </div>
   );
 }
