85000742454c4e5a6019bf132ad04b0d44058aa4
Manonthemon <klimowicz.maciej@gmail.com>
2023-03-17 08:25:54 +0000

    Trying to add UIKit to page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,9 +7,9 @@ function Home() {
     <div>
         <h1>Welcome to JOBCAT</h1>
         <h3>With JobCat you can quickly create a professional resume, apply for your dream job and manage your job applications. Get to work!</h3>
-        <Link to="/resumeForm"><button>Generate Resume</button></Link>
-        <Link to="/jobSearch"><button>Job Search</button></Link>
-        <Link to="/JobApplicationHistory"><button>Application History</button></Link>
+        <Link to="/resumeForm"><button className="uk-button uk-button-default">Generate Resume</button></Link>
+        <Link to="/jobSearch"><button className="uk-button uk-button-default">Job Search</button></Link>
+        <Link to="/JobApplicationHistory"><button className="uk-button uk-button-default">Application History</button></Link>
     </div>
   );
 }
