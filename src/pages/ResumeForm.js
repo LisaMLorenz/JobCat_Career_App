d7cb944a573990992ed4a28b5c1d818289296f93
Manonthemon <klimowicz.maciej@gmail.com>
2023-03-17 09:22:29 +0000

    Added some AIKIT style to the resume

--- src/pages/ResumeForm.js
+++ src/pages/ResumeForm.js
@@ -1,21 +1,29 @@
-import React, { useState } from 'react';
-import Resume from './Resume'
-import ReactDOM from 'react-dom';
-import html2pdf from 'html2pdf.js';
-import UIkit from 'uikit';
-import Icons from 'uikit/dist/js/uikit-icons';
+import React, { useState } from "react";
+import Resume from "./Resume";
+import ReactDOM from "react-dom";
+import html2pdf from "html2pdf.js";
+import UIkit from "uikit";
+import Icons from "uikit/dist/js/uikit-icons";
 
 UIkit.use(Icons);
 
-
-
 function ResumeForm() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [location, setLocation] = useState('');
-  const [education, setEducation] = useState([{ degree: '', institution: '', date: '' }]);
-  const [experience, setExperience] = useState([{ company: '', position: '', startDate: '', endDate: '' , responsibilities: '' }]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [location, setLocation] = useState("");
+  const [education, setEducation] = useState([
+    { degree: "", institution: "", date: "" },
+  ]);
+  const [experience, setExperience] = useState([
+    {
+      company: "",
+      position: "",
+      startDate: "",
+      endDate: "",
+      responsibilities: "",
+    },
+  ]);
   const [skills, setSkills] = useState([""]);
 
   const handleNameChange = (e) => {
@@ -41,7 +49,7 @@ function ResumeForm() {
   };
 
   const handleAddEducation = () => {
-    setEducation([...education, { degree: '', institution: '', date: '' }]);
+    setEducation([...education, { degree: "", institution: "", date: "" }]);
   };
 
   const handleExperienceChange = (index, field, value) => {
@@ -51,7 +59,16 @@ function ResumeForm() {
   };
 
   const handleAddExperience = () => {
-    setExperience([...experience, { company: '', position: '', startDate: '', endDate: '' , responsibilities: ''  }]);
+    setExperience([
+      ...experience,
+      {
+        company: "",
+        position: "",
+        startDate: "",
+        endDate: "",
+        responsibilities: "",
+      },
+    ]);
   };
 
   const handleSkillsChange = (e, index) => {
@@ -66,15 +83,16 @@ function ResumeForm() {
   };
 
   const handleDownload = () => {
-    const element = document.getElementById('newResume');
-    html2pdf().set({ 
-      enableLinks: true,
-      margin: [10, 10],  // This styles the PDF margins
-      filename: 'resume.pdf',
-    }).from(element).save();
-  }
-
-
+    const element = document.getElementById("newResume");
+    html2pdf()
+      .set({
+        enableLinks: true,
+        margin: [10, 10], // This styles the PDF margins
+        filename: "resume.pdf",
+      })
+      .from(element)
+      .save();
+  };
 
   //THIS VERSION OF HANDLE SUMBIT DISPLAS THE READY RESUME ON PAGE (IN ROOT, THINKING HOW TO DISPLAY IT ELSEWHERE)
   const newResume = (e) => {
@@ -86,13 +104,13 @@ function ResumeForm() {
       location,
       education,
       experience,
-      skills
+      skills,
     };
-    ReactDOM.render(<Resume formData={formData} />, document.getElementById('newResume'));
+    ReactDOM.render(
+      <Resume formData={formData} />,
+      document.getElementById("newResume")
+    );
   };
-
-
-
 
   // THIS IS AN ALTERNATIVE FORM HANDLING - It creates a JSON file with data and downloads it.
   // const handleSubmit = (e) => {
@@ -111,152 +129,192 @@ function ResumeForm() {
   //   saveAs(blob, "resume.json");
   // };
 
-
-
   return (
     <div id="resumeContainer">
-    <form>
-      <h2>Personal information</h2>
-      <label>
-        Name:
-        <input type="text" value={name} onChange={handleNameChange} />
-      </label>
-      <br />
-      <label>
-        Email:
-        <input type="email" value={email} onChange={handleEmailChange} />
-      </label>
-      <br />
-      <label>
-        Phone:
-        <input type="text" value={phone} onChange={handlePhoneChange} />
-      </label>
-      <br />
-      <label>
-        Location:
-        <input type="text" value={location} onChange={handleLocationChange} />
-      </label>
-      <br />
-      <h2>Education</h2>
-      {education.map((edu, index) => (
-        <div key={index}>
-          <label>
-            Degree:
-            <input
-              type="text"
-              value={edu.degree}
-              onChange={(e) => handleEducationChange(index, 'degree', e.target.value)}
-            />
-          </label>
-          <br />
-          <label>
-            Institution:
-            <input
-              type="text"
-              value={edu.institution}
-              onChange={(e) => handleEducationChange(index, 'institution', e.target.value)}
-            />
-          </label>
-          <br />
-          <label>
-            Completion date:
-            <input
-              type="date"
-              value={edu.date}
-              onChange={(e) => handleEducationChange(index, 'date', e.target.value)}
-            />
-          </label>
-          <br />
-        </div>
-      ))}
-      <button type="button" onClick={handleAddEducation}>
-        Add education
-      </button>
-      <br/>
-      <h2>Work Experience</h2>
-
-      {experience.map((exp, index) => (
-        <div key={index}>
-          <label>
-            Company:
-            <input
-              type="text"
-              value={exp.company}
-              onChange={(e) => handleExperienceChange(index, 'company', e.target.value)}
-            />
-          </label>
-          <br />
-          <label>
-            Position:
-            <input
-              type="text"
-              value={exp.position}
-              onChange={(e) => handleExperienceChange(index, 'position', e.target.value)}
-            />
-          </label>
-          <br />
-          <label>
-            Start date:
-            <input
-              type="date"
-              value={exp.startDate}
-              onChange={(e) => handleExperienceChange(index, 'startDate', e.target.value)}
-            />
-          </label>
-          <br />
-          <label>
-            End date:
-            <input
-              type="date"
-              value={exp.endDate}
-              onChange={(e) => handleExperienceChange(index, 'endDate', e.target.value)}
-            />
-          </label>
-          <br/>
-          <label>
-            Responsibilities:
-            <input
-              type="text"
-              value={exp.responsibilities}
-              onChange={(e) => handleExperienceChange(index, 'responsibilities', e.target.value)}
-            />
-          </label>
-        </div>
-      ))}
-      <button type="button" onClick={handleAddExperience}>
-        Add work experience
-      </button>
-      <br/>
-
-       <div>
-       <h2>Skills</h2>
-      {skills.map((skill, index) => (
-        <div key={index}>
+      <form className="uk-form-horizontal uk-margin-large-bottom">
+        <h2>Personal information</h2>
+        <label>
+          Name:
           <input
+            className="uk-input"
             type="text"
-            value={skill}
-            onChange={(e) => handleSkillsChange(e, index)}
+            value={name}
+            onChange={handleNameChange}
           />
-        </div>
-      ))}
-      <button onClick={handleAddSkill}>Add Skill</button>
-    </div>
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            className="uk-input"
+            type="email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+        </label>
+        <br />
+        <label>
+          Phone:
+          <input
+            className="uk-input"
+            type="text"
+            value={phone}
+            onChange={handlePhoneChange}
+          />
+        </label>
+        <br />
+        <label>
+          Location:
+          <input
+            className="uk-input"
+            type="text"
+            value={location}
+            onChange={handleLocationChange}
+          />
+        </label>
+        <br />
+        <h2>Education</h2>
+        {education.map((edu, index) => (
+          <div key={index}>
+            <label>
+              Degree:
+              <input
+                className="uk-input"
+                type="text"
+                value={edu.degree}
+                onChange={(e) =>
+                  handleEducationChange(index, "degree", e.target.value)
+                }
+              />
+            </label>
+            <br />
+            <label>
+              Institution:
+              <input
+                className="uk-input"
+                type="text"
+                value={edu.institution}
+                onChange={(e) =>
+                  handleEducationChange(index, "institution", e.target.value)
+                }
+              />
+            </label>
+            <br />
+            <label>
+              Completion date:
+              <input
+                className="uk-input"
+                type="date"
+                value={edu.date}
+                onChange={(e) =>
+                  handleEducationChange(index, "date", e.target.value)
+                }
+              />
+            </label>
+            <br />
+          </div>
+        ))}
+        <button type="button" onClick={handleAddEducation}>
+          Add education
+        </button>
+        <br />
+        <h2>Work Experience</h2>
 
-      <button onClick={newResume}>Generate Resume</button>
-  
-    </form>
-<div>
-    <h2>Your Resume</h2>
-    <div id="newResume"></div>
-    <button onClick={handleDownload}>Download PDF</button>
-     </div>
-   
+        {experience.map((exp, index) => (
+          <div key={index}>
+            <label>
+              Company:
+              <input
+                className="uk-input"
+                type="text"
+                value={exp.company}
+                onChange={(e) =>
+                  handleExperienceChange(index, "company", e.target.value)
+                }
+              />
+            </label>
+            <br />
+            <label>
+              Position:
+              <input
+                className="uk-input"
+                type="text"
+                value={exp.position}
+                onChange={(e) =>
+                  handleExperienceChange(index, "position", e.target.value)
+                }
+              />
+            </label>
+            <br />
+            <label>
+              Start date:
+              <input
+                className="uk-input"
+                type="date"
+                value={exp.startDate}
+                onChange={(e) =>
+                  handleExperienceChange(index, "startDate", e.target.value)
+                }
+              />
+            </label>
+            <br />
+            <label>
+              End date:
+              <input
+                className="uk-input"
+                type="date"
+                value={exp.endDate}
+                onChange={(e) =>
+                  handleExperienceChange(index, "endDate", e.target.value)
+                }
+              />
+            </label>
+            <br />
+            <label>
+              Responsibilities:
+              <input
+                className="uk-input"
+                type="text"
+                value={exp.responsibilities}
+                onChange={(e) =>
+                  handleExperienceChange(
+                    index,
+                    "responsibilities",
+                    e.target.value
+                  )
+                }
+              />
+            </label>
+          </div>
+        ))}
+        <button type="button" onClick={handleAddExperience}>
+          Add work experience
+        </button>
+        <br />
+
+        <div>
+          <h2>Skills</h2>
+          {skills.map((skill, index) => (
+            <div key={index}>
+              <input
+                className="uk-input"
+                type="text"
+                value={skill}
+                onChange={(e) => handleSkillsChange(e, index)}
+              />
+            </div>
+          ))}
+          <button onClick={handleAddSkill}>Add Skill</button>
+        </div>
+        <button onClick={newResume}>Generate Resume</button>
+      </form>
+      <div className="uk-card uk-card-default uk-card-body">
+        <h2 className="uk-card-title">Your Resume</h2>
+        <div  id="newResume"></div>
+        <button className="uk-button"onClick={handleDownload}>Download PDF</button>
+      </div>
     </div>
   );
-
-
-
-
 }
 
 export default ResumeForm;
