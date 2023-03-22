764e5648e202403054d751a26ab735e42c2897e3
LisaMLorenz <hej@frau-lorenz.de>
2023-03-22 19:49:01 +0000

    cat modal centered and confetti on success button

--- src/components/JobApplicationCard.js
+++ src/components/JobApplicationCard.js
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../components/styles/jobHistoryCard.css';
 import $ from 'jquery';
+import Confetti from 'react-confetti';
+
 
 
 function JobApplicationCard(props) {
   const { jobTitle, company, dateApplied, onDelete } = props;
   const [showModal, setShowModal] = useState(false);
   const [catGifUrl, setCatGifUrl] = useState('');
+  const [showConfetti, setShowConfetti] = useState(false);
+
 
   const handleCloseModal = () => {
     setShowModal(false);
@@ -14,6 +18,24 @@ function JobApplicationCard(props) {
   }
 
   const handleGotItClick = () => {
+    setShowConfetti(true); // show confetti
+  }
+
+  useEffect(() => {
+    let timer = null;
+    if (showConfetti) {
+      timer = setTimeout(() => {
+        setShowConfetti(false);
+        onDelete(); // delete card
+      }, 5000);// stop confetti after 5 seconds
+    }
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [showConfetti, onDelete]);
+
+
+  const notGotItClick = () => {
     handleCatGif(); // show modal with cat gif
 
   }
@@ -27,34 +49,43 @@ function JobApplicationCard(props) {
       "url": catQueryURL,
       "method": "GET"
     }).done(function (data) {
-      setCatGifUrl(data.data[0].images.downsized.url);
+      const randomIndex = Math.floor(Math.random() * data.data.length);
+      const randomCatGifUrl = data.data[randomIndex].images.downsized.url;
+      setCatGifUrl(randomCatGifUrl);
       setShowModal(true);
     });
   }
 
   return (
+    
     <div className="jobhistory-card">
+            {showConfetti && <Confetti width={window.innerWidth}/>}
+
       <h2>{jobTitle}</h2>
       <h3>{company}</h3>
       <p>{dateApplied}</p>
       <div className="uk-button-group">
-        <button className="button" onClick={onDelete}>
+        <button className="button" onClick={notGotItClick}>
           Didn't get the job
         </button>
         <button className="button" onClick={handleGotItClick}>
           I got it!
         </button>
       </div>
+      
       {showModal && (
+
         <div className='modal-container'>
           <div className="modal-content">
             <span className="close" onClick={handleCloseModal}>&times;</span>
-            <p>Well done! Here's an amazing cat GIF for you:</p>
+            <h1>Don't be sad!</h1> <p>Here's a funny cat for you. You rock!</p>
             <img src={catGifUrl} alt="cat GIF" />
           </div>
         </div>
+        
       )}
     </div >
+    
   );
 }
 
--- src/pages/handleCatGif.js
+++ src/pages/handleCatGif.js
@@ -1,7 +1,7 @@
 const settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://api.giphy.com/v1/gifs/search?api_key=kOCxdNJFr0qRXkuqLrE3gNuSAIsYsHLW&q=funny+cat&limit=1&offset=0&rating=r&lang=en",
+	"url": "https://api.giphy.com/v1/gifs/search?api_key=kOCxdNJFr0qRXkuqLrE3gNuSAIsYsHLW&q=funny+cat&limit=100&offset=0&rating=r&lang=en",
 	"method": "GET",
 	"headers": {
 		"X-RapidAPI-Key": "a8cd16838dmsh9cbc0d4fbef9db9p1d48f6jsndb619db483a9",
